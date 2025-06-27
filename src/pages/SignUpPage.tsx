0a56267fa05170ae98d32ad8e985f3e27279e87f
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-27 12:26:54 +0530

    Updated code files via regeneration: SignUpPage.tsx

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -53,7 +53,7 @@ const SignUpPage: React.FC = () => {
   );
 
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900">
+    <div className="flex flex-col min-h-screen bg-background">
       <Header />
       <main className="flex-1 flex items-center justify-center p-4">
         <AuthFormWrapper
